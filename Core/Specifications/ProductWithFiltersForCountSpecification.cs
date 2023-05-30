ba679dcac4bb0356bc1a0d66e9fba2ddbbfc8d10
Siarhei Namau <s.naumau@gmail.com>
2023-05-30 19:02:27 +0300

    feat: add pagination

---
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        private readonly ProductSpecParams _productParams;
+
+        public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
+            : base(x => (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
+        {
+            _productParams = productParams;
+        }
+    }
+}
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -34,6 +34,11 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             var query = _storeContext.Set<T>().AsQueryable();
