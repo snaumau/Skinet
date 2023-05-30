a088e8241c90ca9cbbd5b6b1c1c12175f1c7f72b
Siarhei Namau <s.naumau@gmail.com>
2023-05-30 09:59:14 +0300

    feat: add a sorting specification

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -13,6 +13,12 @@ namespace Infrastructure.Data
             if (spec.Criteria != null)
                 query = query.Where(spec.Criteria);
 
+            if (spec.OrderBy != null)
+                query = query.OrderBy(spec.OrderBy);
+
+            if (spec.OrderByDescending != null)
+                query = query.OrderByDescending(spec.OrderByDescending);
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
