3755b161f421dda0809275384770d1145667eb4f
Siarhei Namau <s.naumau@gmail.com>
2023-06-11 19:16:57 +0300

    feat: consume services in the component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { CoreModule } from "./core/core.module";
+import { ShopModule } from "./shop/shop.module";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CoreModule } from "./core/core.module";
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CoreModule
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from "../shared/models/product";
+import { ShopService } from "./shop.service";
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit{
+  products: IProduct[];
 
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit() {
+    this.shopService.getProducts().subscribe(response => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
