b653df9a0c6c9309feec48fd19b37fb8a5f353a3
Siarhei Namau <s.naumau@gmail.com>
2023-06-15 17:07:40 +0300

    feat: add the pagination component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
-import {ShareModule} from "../shared/shared.module";
+import { ShareModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
