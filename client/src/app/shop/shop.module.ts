b6b70f09d73c347e579d937684bdb77dba547237
Siarhei Namau <s.naumau@gmail.com>
2023-06-15 18:43:45 +0300

    feat: add additional component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ShareModule } from "../shared/shared.module";
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
