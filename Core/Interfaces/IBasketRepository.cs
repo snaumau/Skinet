973272124f2445e8f1b75b044d6bf3fda0be149c
Siarhei Namau <s.naumau@gmail.com>
2023-06-19 12:38:20 +0300

    feat: implement the basket repository

--- Core/Interfaces/IBasketRepository.cs
+++ Core/Interfaces/IBasketRepository.cs
@@ -4,9 +4,9 @@ namespace Core.Interfaces;
 
 public interface IBasketRepository
 {
-    Task<CustomerBasket> GetBasketAsync(string basketId);
+    Task<CustomerBasket?> GetBasketAsync(string basketId);
 
-    Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+    Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket);
 
     Task<bool> DeleteBasketAsync(string basketId);
 }
--- Infrastructure/Data/BasketRepository.cs
+++ Infrastructure/Data/BasketRepository.cs
@@ -1,22 +1,40 @@
+using System.Text.Json;
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 
 namespace Infrastructure.Data;
 
 public class BasketRepository : IBasketRepository
 {
-    public Task<CustomerBasket> GetBasketAsync(string basketId)
+    private readonly IDatabase _database;
+    
+    public BasketRepository(IConnectionMultiplexer redis)
     {
-        throw new NotImplementedException();
+        _database = redis.GetDatabase();
+    }
+    
+    public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+    {
+        var data = await _database.StringGetAsync(basketId);
+
+        return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data!);
     }
 
-    public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+    public async Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket)
     {
-        throw new NotImplementedException();
+        var created = await _database.StringSetAsync(basket.Id,
+            JsonSerializer.Serialize(basket),
+            TimeSpan.FromDays(30));
+
+        if (!created)
+            return null;
+
+        return await GetBasketAsync(basket.Id);
     }
 
-    public Task<bool> DeleteBasketAsync(string basketId)
+    public async Task<bool> DeleteBasketAsync(string basketId)
     {
-        throw new NotImplementedException();
+        return await _database.KeyDeleteAsync(basketId);
     }
 }
