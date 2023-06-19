8cd1d8d2b02aed685900000f384d3b3414e391eb
Siarhei Namau <s.naumau@gmail.com>
2023-06-19 21:10:09 +0300

    feat: hook up the product detail component to the basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from "../../shared/models/product";
 import { ShopService } from "../shop.service";
 import { ActivatedRoute } from "@angular/router";
 import { BreadcrumbService } from "xng-breadcrumb";
+import { BasketService } from "../../basket/basket.service";
 
 @Component({
   selector: 'app-product-details',
@@ -11,10 +12,12 @@ import { BreadcrumbService } from "xng-breadcrumb";
 })
 export class ProductDetailsComponent implements OnInit{
   product: IProduct;
+  quantity = 1;
 
   constructor(private shopService: ShopService,
               private activateRoute: ActivatedRoute,
-              private bcService: BreadcrumbService) {
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
     this.bcService.set('@productDetails', ' ');
   }
 
@@ -29,5 +32,19 @@ export class ProductDetailsComponent implements OnInit{
     }, error => {
       console.log(error);
     });
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 }
