6a2d3351a7418d022edfb5e351de5e4ad6102bbc
Siarhei Namau <s.naumau@gmail.com>
2023-06-19 10:35:46 +0300

    feat: clean up loading issues

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -14,7 +14,9 @@ export class ProductDetailsComponent implements OnInit{
 
   constructor(private shopService: ShopService,
               private activateRoute: ActivatedRoute,
-              private bcService: BreadcrumbService) { }
+              private bcService: BreadcrumbService) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
   ngOnInit() {
     this.loadProduct();
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -11,7 +11,7 @@ import { ShopParams } from "../shared/models/shopParams";
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', {static: true}) searchTerm: ElementRef<HTMLInputElement>;
+  @ViewChild('search', {static: false}) searchTerm: ElementRef<HTMLInputElement>;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
