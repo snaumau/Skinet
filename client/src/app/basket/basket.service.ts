f01afd995092a928b5e1f9920b42c8cdd5247ece
Siarhei Namau <s.naumau@gmail.com>
2023-06-19 15:36:03 +0300

    feat: add basket service

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { environment } from "../../enviroments/environment";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, map } from "rxjs";
+import { IBasket } from "../shared/models/basket";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string) {
+    return this.http.get(this.baseUrl + 'basket?id=' + id)
+      .pipe(
+        map((basket: IBasket) => {
+          this.basketSource.next(basket);
+        })
+      );
+  }
+
+  setBasket(basket: IBasket) {
+    return this.http.post(this.baseUrl + 'basket', basket).subscribe((response: IBasket) => {
+      this.basketSource.next(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getCurrentBasketValue() {
+    return this.basketSource.value;
+  }
 }
